e5470290f375751c075ee1826041f7ca807d9188
Hugo Brito <hugobs48@gmail.com>
2024-10-11 16:23:24 -0300

    metodos

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
+
+
+class ContaCorrente{
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            console.log("valor sacado");
+        }
+    }
+}
+
+const contaCorrenteHugo = new ContaCorrente();
+contaCorrenteHugo.saldo = 100;
+
+contaCorrenteHugo.sacar(10);
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
 cliente1.nome = "hugo";
 cliente1.cpf = 12345678911;
-cliente1.agencia = 1;
-cliente1.saldo = 5500;
 cliente1.rg = 98765432111;
 
 cliente2.nome = "ricar";
 cliente2.cpf = 11122233344;
-cliente2.agencia = 2;
-cliente2.saldo = 500;
 
 console.log(cliente1,"\n",cliente2);
