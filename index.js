44422e73c7bc0055b9f7c1dc81eeeb13e34d1099
Hugo Brito <hugobs48@gmail.com>
2024-10-21 16:21:34 -0300

    encapsulamento

--- index.js
+++ index.js
@@ -7,29 +7,26 @@ class Cliente {
 
 class ContaCorrente{
     agencia;
-    saldo;
+    #saldo = 0;
 
     sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-            console.log("valor sacado");
+        if(this.#saldo >= valor){
+            this.#saldo -= valor;
+            console.log("valor sacado: "+ valor + "\nsaldo atual:" + this.#saldo);
         }
+    }
+    depositar(valor){
+        if(this.#saldo >= 0) {
+            this.#saldo += valor;
+            console.log("valor depositado: " + valor+ "\nsaldo atual" + this.#saldo);
+       }
     }
 }
 
 const contaCorrenteHugo = new ContaCorrente();
-contaCorrenteHugo.saldo = 100;
+contaCorrenteHugo.agencia = 1001;
 
+
+contaCorrenteHugo.depositar(150);
 contaCorrenteHugo.sacar(10);
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-cliente1.nome = "hugo";
-cliente1.cpf = 12345678911;
-cliente1.rg = 98765432111;
-
-cliente2.nome = "ricar";
-cliente2.cpf = 11122233344;
-
-console.log(cliente1,"\n",cliente2);
