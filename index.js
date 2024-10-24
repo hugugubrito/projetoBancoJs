f92197f20bd5551b97f5794b4db37868bad18bf9
Hugo Brito <hugobs48@gmail.com>
2024-10-24 16:42:01 -0300

    adicionar classes indiviuais

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-class Cliente {
-    nome;
-    cpf;
-    rg;
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
+//cliente 1
+const cliente1 = new Cliente();
+const conta1 = new ContaCorrente();
 
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
+cliente1.nome = "Hugo";
+cliente1.cpf = 99999988877;
 
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return this._saldo;
-        }
-    }
-    depositar(valor){
-        if(this._saldo < 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-}
+conta1.agencia = 1001;
+conta1.cliente = cliente1;
 
-const contaCorrenteHugo = new ContaCorrente();
-contaCorrenteHugo.agencia = 1001;
+//cliente 2
+const cliente2 = new Cliente();
+const conta2 = new ContaCorrente();
 
+cliente2.nome = "Burno";
+cliente2.cpf = 99988877766;
 
-valorDepositado = contaCorrenteHugo.depositar(150);
-valorSacado = contaCorrenteHugo.sacar(10);
+conta2.agencia = 101;
+conta2.cliente = cliente2;
 
-console.log(valorSacado);
-console.log(contaCorrenteHugo);
+//trabalhando com contas
+
+conta1.depositar(600);
+conta1.sacar(150);
+
+conta1.transferir(200,conta2);
+
+console.log(conta1);
+console.log(conta2);
