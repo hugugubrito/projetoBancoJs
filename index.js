f1730fa76ec353228386ea6bb38053417703b2d5
Hugo Brito <hugobs48@gmail.com>
2024-10-25 15:02:28 -0300

    metodos get e set

--- index.js
+++ index.js
@@ -2,31 +2,24 @@ import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
 //cliente 1
-const cliente1 = new Cliente();
 const conta1 = new ContaCorrente();
+conta1.cliente = new Cliente;
 
-cliente1.nome = "Hugo";
-cliente1.cpf = 99999988877;
-
+conta1.cliente.nome = "Hugo";
+conta1.cliente.cpf = 99999988877;
 conta1.agencia = 1001;
-conta1.cliente = cliente1;
 
 //cliente 2
-const cliente2 = new Cliente();
 const conta2 = new ContaCorrente();
+conta2.cliente = new Cliente();
 
-cliente2.nome = "Burno";
-cliente2.cpf = 99988877766;
-
+conta2.cliente.nome = "Burno";
+conta2.cliente.cpf = 0;
 conta2.agencia = 101;
-conta2.cliente = cliente2;
 
 //trabalhando com contas
 
-conta1.depositar(600);
-conta1.sacar(150);
+console.log(conta1.cliente);
+console.log(conta2.cliente);
 
-conta1.transferir(200,conta2);
-
-console.log(conta1);
-console.log(conta2);
+//console.log(conta2);
